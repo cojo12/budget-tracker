ca8c88f251310dfdf0b039151985c65ce4659b41
Cole Joko <joko.cole.cj@gmail.com>
2020-08-30 20:51:24 -0600

    added db.js

---
+++ public/db.js
@@ -0,0 +1,52 @@
+const { get } = require("mongoose");
+const { response } = require("express");
+
+const request = indexedDB.open("budget", 1);
+
+function checkDatabase() {
+  const transaction = db.transaction(["pending"], "readwrite");
+  const store= transaction.objectStore("pending");
+  const getAll = store.getAll();
+
+  getAll.onsucess = function() {
+    if (getAll.result.length > 0) {
+      fetch("/api/transaction/bulk", {
+        method: "POST",
+        body: JSON.stringify(getAll.result),
+        headers: {
+          Accept: "application/json, text/plain, */*",
+          "Content-Type": "application/json"
+        }
+      }).then(response => response.json())
+      .then(() => {
+        const transaction = db.transaction(["pending"], "readwrite");
+        const store = transaction.objectStore("pending");
+        store.clear();
+      });
+    }
+  };
+}
+
+request.onupgradeneeded = event => {
+  const db = event.target.result;
+  db.createObjectStore("pending", { autoIncrement: true });
+};
+
+request.onsuccess = function(event) {
+  db = event.target.result;
+  if (navigator.onLine) {
+    checkDatabase();
+  }
+};
+
+request.onerror = function(event) {
+  console.log(event.target.errorCode);
+};
+
+function saveRecord(record) {
+  const transaction = db.transaction(["pending"], "readwrite");
+  const store = transaction.objectStore("pending");
+  store.add(record);
+}
+
+window.addEventListener("onling", checkDatabase);
